99cc588e045f2854ff078b0fd3a8caa0df480028
abheek28 <abheekchoudhury28@gmail.com>
2026-02-26 12:37:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-document.querySelectorAll(".nav-links span[data-section]").forEach(btn => {
-  btn.addEventListener("click", () => {
-    document.querySelectorAll(".content-section").forEach(sec => {
-      sec.style.display = "none";
-    });
-    document.getElementById(btn.dataset.section).style.display = "block";
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-});
-
-document.querySelectorAll(".item-title").forEach(title => {
-  title.addEventListener("click", () => {
-    title.parentElement.classList.toggle("active");
+document.querySelectorAll(".title").forEach(item => {
+  item.addEventListener("click", () => {
+    item.parentElement.classList.toggle("active");
   });
 });
 
 document.getElementById("theme-toggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+  document.body.classList.toggle("light");
 });
