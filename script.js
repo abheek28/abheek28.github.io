b739f55faab0eab9ac4c0dfbbd3f2796206abf93
abheek28 <abheekchoudhury28@gmail.com>
2026-02-26 11:54:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Dark / Light Toggle
+const toggle = document.getElementById("themeToggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
+// Scroll Animation
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = {
+  threshold: 0.2
+};
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add("visible");
+    observer.unobserve(entry.target);
+  });
+}, appearOptions);
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
