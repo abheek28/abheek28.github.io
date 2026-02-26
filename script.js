99db4a6057b5b85d6a5d95649bab7cfafc82b424
abheek28 <abheekchoudhury28@gmail.com>
2026-02-26 12:21:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-// NAVIGATION CLICK
-const navItems = document.querySelectorAll(".nav-item");
-const sections = document.querySelectorAll(".content-section");
+const toggle = document.getElementById("themeToggle");
 
-navItems.forEach(item => {
-  item.addEventListener("click", () => {
-    const target = item.dataset.section;
-
-    sections.forEach(section => {
-      section.style.display = section.id === target ? "block" : "none";
-    });
-
-    window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
-  });
-});
-
-// EXPAND DESCRIPTION
-const clickables = document.querySelectorAll(".clickable");
-
-clickables.forEach(title => {
-  title.addEventListener("click", () => {
-    const desc = title.nextElementSibling;
-    desc.classList.toggle("active");
-  });
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
 });
