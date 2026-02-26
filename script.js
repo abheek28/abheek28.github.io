1572a2843bde57ee314bb8427c673ff823fd6c62
abheek28 <abheekchoudhury28@gmail.com>
2026-02-26 12:11:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-// Dark / Light Toggle
+// DARK MODE TOGGLE
 const toggle = document.getElementById("themeToggle");
+
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
 });
-// Scroll Animation
-const faders = document.querySelectorAll(".fade-in");
-const appearOptions = {
-  threshold: 0.2
-};
-const appearOnScroll = new IntersectionObserver(function(entries, observer) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) return;
-    entry.target.classList.add("visible");
-    observer.unobserve(entry.target);
+
+// ACCORDION ANIMATION
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+accordionHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    const item = header.parentElement;
+
+    item.classList.toggle("active");
+
+    accordionHeaders.forEach(otherHeader => {
+      if (otherHeader !== header) {
+        otherHeader.parentElement.classList.remove("active");
+      }
+    });
   });
-}, appearOptions);
-faders.forEach(fader => {
-  appearOnScroll.observe(fader);
 });
